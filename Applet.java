2d4ce52f3150c8b8486fdc5136d70510be768f11
Amaya M <122987613+amayamohan@users.noreply.github.com>
2025-08-22 09:54:00 +0530

    Add files via upload

---
+++ Applet.java
@@ -0,0 +1,13 @@
+import java.applet.Applet;
+import java.awt.Graphics;
+public class Shape  extends Applet {
+	public void paint(Graphics g) {
+		 g.drawOval(20, 20, 200, 120);
+		 g.drawLine(180, 150, 180,370);
+		 g.drawLine(180, 150, 440,370);
+		 g.drawLine(180, 370, 440,370);
+		 
+	 }
+
+
+}
